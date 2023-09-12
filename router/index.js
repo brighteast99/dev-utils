bf144fd4288a64537bcf10933758473c5f8ee5d0
brighteast99 <brighteast99@gmail.com>
2023-09-12 14:37:00 +0900

    ✨ 유니코드 변환 기능 추가
    유니코드를 일반 문자로 변환하는 기능 추가

--- router/index.js
+++ router/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 
 router.get('/api', (req, res) => res.render('api'))
+router.get('/unicode', (req, res) => res.render('unicode'))
 
 module.exports = router
