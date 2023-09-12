49c6aa911a6d7b51614111e2cf841a4d91b6ecbb
brighteast99 <brighteast99@gmail.com>
2023-09-12 15:09:01 +0900

    💄 상단 내비게이션 추가, 메인 페이지 추가

--- router/index.js
+++ router/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 
+router.get('/', (req, res) => res.render('index'))
 router.get('/api', (req, res) => res.render('api'))
 router.get('/unicode', (req, res) => res.render('unicode'))
 
